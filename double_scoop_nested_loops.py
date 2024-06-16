f33746abef5b09e0e66788de2156ba324e27710c
Silas Aurelius <162638735+SilasAurelius@users.noreply.github.com>
2024-06-16 18:45:55 -0400

    Add files via upload

---
+++ double_scoop_nested_loops.py
@@ -0,0 +1,18 @@
+"""
+Task 1: Your Mood Tracker
+Simulate a mood tracker that records your mood at three different times of the day (morning, afternoon, evening) for each day of the week. Use nested loops to implement this: the outer loop should iterate over the days, and the inner loop should iterate over the times of the day. For each time, randomly select a mood from a predefined list and print it.
+Example Outcome: An example would be "On Tuesday afternoon you were sad" "On Tuesday night you were happy" "On Wednesday morning you were tired"
+"""
+import random
+days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+times = ['morning', 'afternoon', 'evening']
+moods = ['happy', 'sad', 'energetic', 'calm']
+
+
+random.shuffle(moods)
+
+for i in days:
+    print("Today is: " + i)
+    if i == 'Sunday':
+        for x in times:
+           pass
