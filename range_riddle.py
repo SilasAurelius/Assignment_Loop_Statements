0d049703da29d51ae41100f4fe55ddccf3e53b8f
Silas Aurelius <162638735+SilasAurelius@users.noreply.github.com>
2024-06-19 21:33:26 -0400

    Update range_riddle.py

--- range_riddle.py
+++ range_riddle.py
@@ -1,15 +1,36 @@
 """
-Task 1: Your Mood Today
-Write a program that prints off different moods for each day of the week. Create a list of moods such as "Happy", "Sad", "Energetic", and "Calm". Using the range() function, loop through the days of the week and for each day, randomly select a mood from the list and print it. Ensure that your program includes the use of the random module to select the mood.
+Task 1: The for Loop DJ Set
+Using the provided genres list, write a for loop that prints out each genre with a custom message. Extend this task by adding a counter that displays the number of the track before the genre.
 """
-import random
-week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-mood = ['happy', 'sad', 'energetic', 'calm', 'happy', 'over-joyed', 'tired']
 
-mood_rand = mood
-random.shuffle(mood)
-
-for i in range(0, 7, 1):
-    print(f"Today is: " + str(week[i]) + " and you were: " +  (mood_rand[i]))
-
-
+# Our playlist of genres
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+counter = 1
+"""for i in genres:
+    print(f"Current track: {counter} Genre {i}.")
+    counter += 1
+"""
+"""
+Task 2: The Remix Artist with while
+Convert the for loop from Task 1 into a while loop. Ensure it performs the same function but also includes a condition to stop the loop if a certain genre is played for instance Hip-hop.
+"""
+"""
+while counter < 3:
+    for i in genres:
+        print(f"Current track: {counter} Genre {i}.")
+        counter += 1
+        if i == 'Hip-hop':
+            break
+"""
+"""
+Task 3: Light Show Technician Loop
+Using the range() function, loop through the genres list by index. For each genre, print out the track number and a message that the light show is ready. Modify the loop to skip a genre if it's not suitable for the light show, for instance Classical genre.
+"""
+for i in range(4):
+    if i == 0:
+        continue
+    if i == 3:
+        continue
+    print(f"Track:{i} - {str(genres[i])} - Ready for a light show!")
+    
+    
